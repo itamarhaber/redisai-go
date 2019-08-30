9d9ee073714294cc649827cbb2b8f16322d01548
filipecosta90 <filipecosta.90@gmail.com>
2019-08-30 15:12:57 +0100

    [add] implemented all commands on pipelinedClient

--- redisai/pipelinedClient.go
+++ redisai/pipelinedClient.go
@@ -34,41 +34,121 @@ func (c *PipelinedClient) TensorGet(name string, ct TensorContentType) (data []i
 	return data, nil
 }
 
-func (c *PipelinedClient) ModelSet(name string, backend BackendType, device DeviceType, data []byte, inputs []string, outputs []string) error {
-	panic("implement me")
+func (c *PipelinedClient) ModelSet(name string, backend BackendType, device DeviceType, data []byte, inputs []string, outputs []string) ( err error) {
+	args := redis.Args{}.Add(name, backend, device)
+	if len(inputs) > 0 {
+		args = args.Add("INPUTS").AddFlat(inputs)
+	}
+	if len(outputs) > 0 {
+		args = args.Add("OUTPUTS").AddFlat(outputs)
+	}
+	args = args.Add(data)
+
+	if c.ActiveConn == nil {
+		c.ActiveConn = c.Pool.Get()
+		defer c.ActiveConn.Close()
+	}
+	err = c.ActiveConn.Send("AI.MODELSET", args...)
+	if err != nil {
+		return err
+	}
+	// incremement the pipeline
+	// flush if required
+	err = c.pipeIncr(c.ActiveConn)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (c *PipelinedClient) ScriptSet(name string, device DeviceType, data string) error {
-	panic("implement me")
+func (c *PipelinedClient) ScriptSet(name string, device DeviceType, script_source string) (err error) {
+	args := redis.Args{}.Add(name, device, script_source)
+	err = c.SendAndIncr("AI.SCRIPTSET" , args)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (c *PipelinedClient) ScriptRun(name string, fn string, inputs []string, outputs []string) error {
-	panic("implement me")
+func (c *PipelinedClient) SendAndIncr( commandName string, args redis.Args) (err error) {
+	if c.ActiveConn == nil {
+		c.ActiveConn = c.Pool.Get()
+	}
+	err = c.ActiveConn.Send(commandName, args...)
+	if err != nil {
+		return err
+	}
+	// incremement the pipeline
+	// flush if required
+	err = c.pipeIncr(c.ActiveConn)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func (c *PipelinedClient) ScriptRun(name string, fn string, inputs []string, outputs []string) (err error) {
+	args := redis.Args{}.Add(name, fn)
+	if len(inputs) > 0 {
+		args = args.Add("INPUTS").AddFlat(inputs)
+	}
+	if len(outputs) > 0 {
+		args = args.Add("OUTPUTS").AddFlat(outputs)
+	}
+	err = c.SendAndIncr("AI.SCRIPTRUN" , args)
+	if err != nil {
+		return err
+	}
+	return
 }
 
 func (c *PipelinedClient) ModelGet(name string) (data []interface{}, err error) {
-	panic("implement me")
+	args := redis.Args{}.Add(name)
+	err = c.SendAndIncr("AI.MODELGET" , args)
+	if err != nil {
+		return data, err
+	}
+	return
 }
 
 func (c *PipelinedClient) ModelDel(name string) (err error) {
-	panic("implement me")
+	args := redis.Args{}.Add(name)
+	err = c.SendAndIncr("AI.MODELDEL" , args)
+	if err != nil {
+		return err
+	}
+	return
 }
 
 func (c *PipelinedClient) ScriptGet(name string) (data []interface{}, err error) {
-	panic("implement me")
+	args := redis.Args{}.Add(name)
+	err = c.SendAndIncr("AI.SCRIPTGET" , args)
+	if err != nil {
+		return data, err
+	}
+	return
 }
 
 func (c *PipelinedClient) ScriptDel(name string) (err error) {
-	panic("implement me")
+	args := redis.Args{}.Add(name)
+	err = c.SendAndIncr("AI.SCRIPTDEL" , args)
+	if err != nil {
+		return err
+	}
+	return
 }
 
-func (c *PipelinedClient) LoadBackend(backendIdentifier BackendType, location string) (err error) {
-	panic("implement me")
+func (c *PipelinedClient) LoadBackend(backend_identifier BackendType, location string) (err error) {
+	args := redis.Args{}.Add("LOADBACKEND").Add(backend_identifier).Add(location)
+	err = c.SendAndIncr("AI.CONFIG" , args)
+	if err != nil {
+		return err
+	}
+	return
 }
 
 // ConnectPipelined intializes a Client with pipeline enabled by default
 func ConnectPipelined(url string, pipelineMax int, pool *redis.Pool) (c *PipelinedClient) {
-
 	var cpool *redis.Pool = nil
 	if pool == nil {
 		cpool = &redis.Pool{
@@ -114,17 +194,7 @@ func (c *PipelinedClient) Close() (err error) {
 // ModelRun runs a RedisAI model
 func (c *PipelinedClient) ModelRun(name string, inputs []string, outputs []string) (err error) {
 	args := ModelRunArgs(name, inputs, outputs, false)
-	if c.ActiveConn == nil {
-		c.ActiveConn = c.Pool.Get()
-		defer c.ActiveConn.Close()
-	}
-	err = c.ActiveConn.Send("AI.MODELRUN", args...)
-	if err != nil {
-		return err
-	}
-	// incremement the pipeline
-	// flush if required
-	err = c.pipeIncr(c.ActiveConn)
+	err = c.SendAndIncr("AI.MODELRUN" , args)
 	if err != nil {
 		return err
 	}
@@ -134,19 +204,7 @@ func (c *PipelinedClient) ModelRun(name string, inputs []string, outputs []strin
 // TensorSet sets a tensor
 func (c *PipelinedClient) TensorSet(name string, dt DataType, shape []int, data interface{}) (err error) {
 	args,err := TensorSetArgs(name, dt, shape, data, false)
-	if err != nil {
-		return err
-	}
-	if c.ActiveConn == nil {
-		c.ActiveConn = c.Pool.Get()
-	}
-	err = c.ActiveConn.Send("AI.TENSORSET", args...)
-	if err != nil {
-		return err
-	}
-	// incremement the pipeline
-	// flush if required
-	err = c.pipeIncr(c.ActiveConn)
+	err = c.SendAndIncr("AI.TENSORSET" , args)
 	if err != nil {
 		return err
 	}
